f31f2617f2e984c7dfdf5d5fa8b4aad5aeedb629
xwx5683 <111966604+xwx5683@users.noreply.github.com>
2024-09-02 17:26:45 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -12,19 +12,19 @@ const TEMPLATE_CONFIG = [
       
       城市：{{city.DATA}}
       
-      天气：{{weather.DATA}}
+      天气☁️：{{weather.DATA}}
       
-      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
+      气温🌡️(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
       
       风向: {{wind_direction.DATA}}
       
-      风级: {{wind_scale.DATA}}
+      风级🌬️: {{wind_scale.DATA}}
       
       {{comprehensive_horoscope.DATA}}
       
       ---
       
-      今天是我们相识的第{{love_day.DATA}}天
+      📅今天是我们相识的第{{love_day.DATA}}天
       
       {{birthday_message.DATA}}
       
