3347809fb31f4323c6a8dc3eb7146b51372d32d3
xwx5683 <111966604+xwx5683@users.noreply.github.com>
2024-09-02 14:12:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '♥我的宝贝专属♥',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'qjU14hz_-uZFxs1IUwBjJC1Yntywtw5dbj_JoHQE7vw',
+      id: 'o3Fa16lO9NUBQ_Ksis_oXGNaL8i0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
