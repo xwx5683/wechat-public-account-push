368724ff80255897b793005120041417b5be12f8
xwx5683 <111966604+xwx5683@users.noreply.github.com>
2024-09-02 15:32:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -46,7 +46,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'UWUdLfcF1hhsndu1jbr30m0F0r4ODW1qYM-IUh3Hv6w',
 
   CALLBACK_USERS: [
     {
