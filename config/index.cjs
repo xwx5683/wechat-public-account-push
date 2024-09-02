22aa60bb5b67d4c476f1360fe8011f6c703e30d8
xwx5683 <111966604+xwx5683@users.noreply.github.com>
2024-09-02 14:18:54 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o3Fa16lO9NUBQ_Ksis_oXGNaL8i0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'qjU14hz_-uZFxs1IUwBjJC1Yntywtw5dbj_JoHQE7vw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-27',
       festivals: [
