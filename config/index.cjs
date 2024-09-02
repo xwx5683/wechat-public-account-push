f821f3388514b723fd719debf1454b230d084ac9
xwx5683 <111966604+xwx5683@users.noreply.github.com>
2024-09-02 17:34:57 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,21 +8,12 @@
 const USER_CONFIG = {
   // 请将以下代码按照图示粘贴到对应位置
   USE_PASSAGE: 'push-deer',
-  // 使用微信测试号：公众号APP_ID
-  APP_ID: 'wx3794c7661f9157a5',
-
-  // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '5c10d8de1e0372181c0c83dc2e28f863',
 
   PROVINCE: '上海',
   CITY: '杨浦',
 
   USERS: [
     {
-      // 想要发送的人的名字
-      name: '♥我的宝贝专属♥',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU31814TmcwTKEJzuYMYR3GARF2GHiUlQ6tRl0Ux',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
